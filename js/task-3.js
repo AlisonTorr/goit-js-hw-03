17db9407a3e7280dc1ba8f84746d1fff643dd581
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-01-25 21:57:14 +0300

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,45 @@
+const findBestEmployee = function (employees) {
+  const values = Object.values(employees);
+  console.log(values);
+
+  for (let i = 1; i < values.length; i += 1) {
+    let maxTasks = value[0];
+    if (values[i] > maxTasks) {
+      maxTasks = values[i];
+    }
+    let index = values.indexOf(maxTasks);
+
+    const keys = Object.keys(employees);
+    console.log(keys);
+
+    const bestEmployee = keys[index];
+  }
+
+  return bestEmployee;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
