d6cb0f80ba2ac6f849062e750f69530935c41e8d
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-01 12:05:27 +0300

    Add files via upload

--- js/task-5.js
+++ js/task-5.js
@@ -7,12 +7,13 @@ const products = [
 
 const getAllPropValues = function (arr, prop) {
   const propArr = [];
+  let resultArr = [];
 
   for (const product of products) {
     const keys = Object.keys(product);
     for (const key of keys) {
       if (key === prop) {
-        const resultArr = propArr.push(product[key]);
+        resultArr = propArr.push(product[key]);
       }
     }
 
